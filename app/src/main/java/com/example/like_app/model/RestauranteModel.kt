3d10ce23e44752e27321af8e7234c8c6090a7aa4
luis0023 <21200022@ue.edu.pe>
2023-11-01 12:59:53 -0500

    Se modifica el fragment_list_rest para implementarlo con el firebase

--- app/src/main/java/com/example/like_app/model/RestauranteModel.kt
+++ app/src/main/java/com/example/like_app/model/RestauranteModel.kt
@@ -1,3 +1,3 @@
 package com.example.like_app.model
 
-data class RestauranteModel( val id:Int, val nombre:String, val tiempo:String, val precioEnvio:String, val imgRest:Int)
+data class RestauranteModel(  val nombre:String, val tiempo:String, val precioEnvio:String, val imageUrl:String)
