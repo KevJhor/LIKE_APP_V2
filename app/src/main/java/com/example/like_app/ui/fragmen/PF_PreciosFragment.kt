6933382aa60a2e90c9e6ec620bec433269d11578
laboratorioesan <20100767@ue.edu.pe>
2023-11-04 12:27:28 -0500

    PreguntasFrecuentes

---
+++ app/src/main/java/com/example/like_app/ui/fragmen/PF_PreciosFragment.kt
@@ -0,0 +1,21 @@
+package com.example.like_app.ui.fragmen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.like_app.R
+
+class PF_PreciosFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_p_f__precios, container, false)
+    }
+
+}
