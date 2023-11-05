77ea582b97322edc31a0aed16e97843d51bdb9cc
Edson <18100620@ue.edu.pe>
2023-11-05 18:22:23 -0500

    rename activity_addcard.xml + activity_deposito.xml and activity_pay_online.xml and created class AddcardActivity.kt + DepositoActivity.kt + PayonlineActivity.kt

---
+++ app/src/main/java/com/example/like_app/ui/payonline/AddcardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.like_app.ui.payonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.like_app.R
+
+class AddcardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addcard)
+    }
+}
