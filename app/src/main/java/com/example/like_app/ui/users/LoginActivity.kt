99730c0b9adf49af1a3982ed0618dd2d15e124c9
Edson <18100620@ue.edu.pe>
2023-11-10 23:17:49 -0500

    LoginActivity.kt y login_activity.xml implementacion de ingreso auth.Firebase

--- app/src/main/java/com/example/like_app/ui/users/LoginActivity.kt
+++ app/src/main/java/com/example/like_app/ui/users/LoginActivity.kt
@@ -4,16 +4,53 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.like_app.MainActivity
 import com.example.like_app.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        // Logeo con correo y password
+        val auth = FirebaseAuth.getInstance()
+
+        val etUser: EditText = findViewById(R.id.etUser)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val correo = etUser.text.toString()
+            val clave = etPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(correo,clave)
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Inicio de sesión exitoso"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }else {
+                        Snackbar
+                            .make(
+                                findViewById(android.R.id.content)
+                                ,"Credenciales inválidas"
+                                , Snackbar.LENGTH_LONG
+                            ).show()
+                    }
+                }
+        }
+
 
 
         // Inicializar los botones y el TextView
